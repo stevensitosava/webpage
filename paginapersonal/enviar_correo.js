3d657052182ce0441954de59e28b4bdd7d562326
stevensitosava <jeusuarioestandar@gmail.com>
2024-06-24 20:30:09 +0200

    Add files via upload

---
+++ paginapersonal/enviar_correo.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("contact-form");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        
+        const nombre = document.getElementById("nombre").value;
+        const email = document.getElementById("email").value;
+        const tema = document.getElementById("tema").value;
+        const mensaje = document.getElementById("mensaje").value;
+
+        // Validar que los campos requeridos no estén vacíos
+        if (nombre && email && tema && mensaje) {
+            const correoDestino = "srssdesing@gmail.com";
+            const asunto = `Nuevo mensaje de ${nombre}`;
+            const cuerpo = `Nombre: ${nombre}\nEmail: ${email}\nTema: ${tema}\nMensaje: ${mensaje}`;
+
+            const url = `https://script.google.com/macros/s/AKfycbwY7Wte1jiWPJ3EnMw4tOnEep3CMljOcMPz9DX4gWYwC6Kc8pKXU_yPa0eTunMF-2Ob/exec?to=${correoDestino}&subject=${encodeURIComponent(asunto)}&body=${encodeURIComponent(cuerpo)}`;
+
+            fetch(url, { method: "GET" })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.result === "success") {
+                        const mensajeConfirmacion = document.getElementById("mensaje-confirmacion");
+                        mensajeConfirmacion.style.display = "block";
+                        form.reset();
+                        setTimeout(function () {
+                            mensajeConfirmacion.style.display = "none";
+                        }, 5000);
+                    } else {
+                        alert("There was an error sending the message. Please try again later.");
+                    }
+                })
+                .catch(error => {
+                    console.error("Error:", error);
+                    alert("There was an error sending the message. Please try again.");
+                });
+        } else {
+            alert("Please complete all required fields.");
+        }
+    });
+});
---
+++ paginapersonal/script.js
@@ -0,0 +1,64 @@
+
+function seleccionar(link) {
+    var opciones = document.querySelectorAll('#links  a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    link.className = "seleccionado";
+
+    var x = document.getElementById("nav");
+    x.className = "";
+}
+
+function responsiveMenu() {
+    var x = document.getElementById("nav");
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
+}
+
+window.onscroll = function() { efectoHabilidades() };
+
+function efectoHabilidades() {
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if (distancia_skills >= 200) {
+        document.getElementById("html").classList.add("barra-progreso1");
+        document.getElementById("js").classList.add("barra-progreso2");
+        document.getElementById("bd").classList.add("barra-progreso3");
+        document.getElementById("ps").classList.add("barra-progreso4");
+    }
+
+}
+
+function isElementInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+
+const yearElement = document.getElementById('year');
+const currentYear = new Date().getFullYear();
+yearElement.textContent = currentYear;
+
+
+
+
+
+
+
+
+
+
+  
+  
+
